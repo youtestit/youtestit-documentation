d255977af3d8b86c513f898f71082654a089a4f6
Patrick Guillerm <patrickguillerm@gmail.com>
2011-11-06 22:47:47 +0100

    add documentation on eclipse environnement

---
+++ development/fr/includes/src/eclipse.sh
@@ -0,0 +1,16 @@
+JAVA_HOME=$HOME/soft/java/jdk/jdk1.6.0_24
+M2_HOME=$HOME/soft/java/apache-maven-3.0.3
+M2=$M2_HOME/bin
+
+
+export JAVA_HOME=$JAVA_HOME
+export M2=$M2
+export M2_HOME=$M2_HOME
+export PATH=$PATH:$JAVA_HOME
+export PATH=$PATH:$JAVA_HOME/bin
+export PATH=$PATH:$M2
+export PATH=$PATH:$M2_HOME/bin
+export GIT_SSH=/usr/bin/ssh
+
+$HOME/soft/java/eclipse-indigo/./eclipse
+
